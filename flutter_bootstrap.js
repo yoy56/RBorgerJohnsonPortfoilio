23bd59c87ace67dd2a62aa2e09b85ad77baf53c0
yoy56 <36109751+yoy56@users.noreply.github.com>
2026-07-17 04:41:19 +0000

    Deploying to gh-pages from @ yoy56/RBorgerJohnsonPortfoilio@f41f2be46b732f29f0ed4add1f94c4cfc757e271 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2175127462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1320887825" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
